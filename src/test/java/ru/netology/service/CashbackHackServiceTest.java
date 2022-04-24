f2397a61468b9ee04e9b6ce5e07fa34ff8456405
lesdead <lesdead@gmail.com>
2022-04-24 03:39:02 +0300

    LastFinish testng project

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,12 +6,33 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void testRemain() {
+    public void shouldIfAmountUnder1000By1() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldIfAmountOver1000By1() {
         CashbackHackService service = new CashbackHackService();
 
         int expected = 999;
         int actual = service.remain(1001);
 
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
+
+    @Test
+    public void shouldIfAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1000;
+        int actual = service.remain(1000);
+
+        assertEquals(expected,actual);
+    }
+
 }
